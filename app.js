f213acc9f23e360203abcc13b72ffba0769a2298
ivanmarquez99 <ivanaurelio.s258702@cesurformacion.com>
2023-03-13 10:59:22 +0100

    Añadido de comentarios

--- app.js
+++ app.js
@@ -5,31 +5,16 @@ var templateList = document.querySelector("template.list");
 var templateModal = document.querySelector("template.modal");
 var body = document.querySelector("body");
 
+// Añadimos el mapa de open street map
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
+// Añadimos la vista con las coordenadas de Málaga y un zoom de 16
 map.setView([36.7201600, -4.4203400], 16)
 
-const options = {
-    enableHighAccuracy: true,
-    maximumAge: 30000,
-    timeout: 27000
-};
-
-function error(err) {
-    console.log(err)
-}
-
-function geoFindMe() {
-    if (!navigator.geolocation) {
-        console.log('No permitido por el navegador')
-    } else {
-        navigator.geolocation.getCurrentPosition(showInfo, error, options);
-    }
-}
-
+// Creamos un array donde importaremos los datos del json
 var elementos = []
 
 window.onload=function() {
@@ -47,18 +32,21 @@ window.onload=function() {
             elementos.push(line);
           });
         }
+        //Llamamos a las funciones de crear marcadores y crear la lista
         crearMarcadores()
         crearLista()
       });  
 }
 
 function crearMarcadores() {
+    // Recorremos el listado de elementos para crear los marcadores en el mapa
     elementos.forEach(function(e) {
         var latitud = e.properties.x;
         var longitude = e.properties.y;
         var name = e.properties.nombre;
         var calle = e.properties.direccion;
 
+        // Creamos los marcadores
         let marker = L.marker([latitud, longitude]).addTo(map);
         marker.bindPopup('<p class="fs-6">'+name+'</p><p>'+calle+'</p>').addTo(map);
     })
@@ -66,8 +54,10 @@ function crearMarcadores() {
 
 function crearLista() {
     elementos.forEach(function(e, i) {
-
+    
+    // Creamos el objeto en la lista con los datos oportunos
     let nuevoEdif = templateList.content.cloneNode(true);
+    // Evento de click para que se abra el modal
     nuevoEdif.querySelector('.element').addEventListener("click", function()
     {
         const myModal = new bootstrap.Modal(document.getElementById('modal-'+i));
@@ -80,7 +70,7 @@ function crearLista() {
     tabla.appendChild(nuevoEdif);
 
     
-
+    // Creamos el modal con los datos oportunos
     let nuevoModal =  templateModal.content.cloneNode(true);
     nuevoModal.querySelector(".modal").setAttribute('id', 'modal-'+i)
     nuevoModal.querySelector(".modal-title").innerText = e.properties.nombre;
@@ -91,6 +81,5 @@ function crearLista() {
     
     })
 }
-console.log(elementos)
 
 
